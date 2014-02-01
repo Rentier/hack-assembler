5eef2858b6935e5378592e861f3ec7277400acf7
Jan-Christoph Klie <rentier42@googlemail.com>
2014-02-01 12:59:04 +0100

    c-instruction calc twerks

--- src/hasm/parser.clj
+++ src/hasm/parser.clj
@@ -2,13 +2,18 @@
 
 (def not-nil? (complement nil?))
 
+(def dest #{"null" "M" "D" "MD" "A" "AM" "AD" "AMD"})
+(def comp #{"0" "1" "-1" "D" "A" "!D" "!A" "-D" "-A" "D+1" 
+           "A+1" "D-1" "A-1" "D+A" "D-A" "A-D" "D&A" "D|A"})
+
 (defn is-c-calc? [s]
   "A C-instruction for calculaion can be one of the following:
    dest=x+y dest=x-y dest=x dest=0 dest=1 dest=-1
 
    With x={A,D,M} y={A,D,M,1} dest={A,D,M,MD,A,AM,AD,AMD,null}"
-  (not-nil? 
-   (re-matches #"(A|D|M|MD|A|AM|AD|AMD|null)=([ADM][\+-][ADM1])|(0|1|-1)" s)))
+  (let [[lhs rhs] (clojure.string/split s #"=" 2)]
+    (and (contains? dest lhs)
+         (contains? comp rhs))))
  
 (defn is-a-symbol? [s]
   (re-matches #"@[a-zA-Z\$_\.][0-9a-zA-Z\$_\.]*" s))

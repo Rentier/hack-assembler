23f25cd36e0c64eae3dc99d03d5fe6eed76e9e2d
Jan-Christoph Klie <rentier42@googlemail.com>
2014-02-01 17:46:39 +0100

    Split stuff works

--- src/hasm/parser.clj
+++ src/hasm/parser.clj
@@ -7,24 +7,36 @@
                           "A+1" "D-1" "A-1" "D+A" "D-A" "A-D" "D&A" "D|A"})
 (def c-instruction-jump #{"null" "JGT" "JEQ" "JGE" "JLT" "JNE" "JLE" "JMP"})
 
-(defn is-c-calc-instruction? [s]
+(defn split-calc-instruction [s]
+  "Splits a c-instruction of form dest=comp into a its atoms"
+  (clojure.string/split s #"=" 2))
+
+(defn split-jump-instruction [s]
+  "Splits a c-instruction of form dest;jump into its atoms"
+  (clojure.string/split s #";" 2))
+
+(defn split-a-instruction [s]
+  "Returns the payload S of an a-instructions in the form of @S"
+  (subs s 1))
+
+(defn calc-instruction? [s]
   "A c-instruction for calculaion has the form of dest=comp"
-  (let [[lhs rhs] (clojure.string/split s #"=" 2)]
+  (let [[lhs rhs] (split-calc-instruction s)]
     (and (contains? c-instruction-dest lhs)
          (contains? c-instruction-comp rhs))))
 
-(defn is-c-jump-instruction? [s]
+(defn jump-instruction? [s]
   "A c-instruction for jumping has the form of dest;jump"
-  (let [[lhs rhs] (clojure.string/split s #";")]
+  (let [[lhs rhs] (split-jump-instruction s)]
     (and (contains? c-instruction-dest lhs)
          (contains? c-instruction-jump rhs))))
 
-(defn is-a-const-instruction? [s]
+(defn const-instruction? [s]
   "A a-instruction representing a constant has the form of @N,
    where N is a non-negative integer"
   (not-nil? (re-matches #"@\d+" s)))
 
-(defn is-a-symbol-instruction? [s]
+(defn symbol-instruction? [s]
   "A a-instruction representing a symbol has the form of @S,
    where S can be any sequence of letters, digits, underscores,
    dot, dollar sign and colon that does not start with a digit"
--- test/hasm/test_parser.clj
+++ test/hasm/test_parser.clj
@@ -2,46 +2,65 @@
   (:require [clojure.test :refer :all]
             [hasm.parser :refer :all]))
 
-(deftest recognize-c-calc-instructions
+(deftest split-calc-instructions
+  "Checks whether calc-instructions are split the right way"
+  (is (split-calc-instruction "D=M") ["D" "M"])
+  (is (split-calc-instruction "null=null") ["null" "null"])
+  (is (split-calc-instruction "M=!A") ["M" "!A"])
+  (is (split-calc-instruction "D=D&A") ["D" "D&A"]))
+
+(deftest split-jump-instructions
+  "Checks whether jump-instructions are split the right way"
+  (is (split-jump-instruction "null;JGT") ["null" "JGT"])
+  (is (split-jump-instruction "AMD;JMP") ["AMD" "JMP"])
+  (is (split-jump-instruction "MD;JGE") ["MD" "JGE"])
+  (is (split-jump-instruction "M;JEQ") ["M" "JEQ"]))
+
+(deftest split-a-instructions
+  "Checks whether a-instructions are split the right way"
+  (is (split-a-instruction "@FOO") "FOO")
+  (is (split-a-instruction "@123") "123"))  
+
+(deftest recognize-calc-instructions
   "Checks whether all possible c-calc-instructions are recognized."
   (doseq [lhs ["null" "M" "D" "MD" "A" "AM" "AD" "AMD"]]
     (doseq [rhs ["0" "1" "-1" "D" "A" "!D" "!A" "-D" "-A" "D+1" 
                  "A+1" "D-1" "A-1" "D+A" "D-A" "A-D" "D&A" "D|A"]]
-      (is (is-c-calc-instruction? (str lhs "=" rhs))))))
+      (is (calc-instruction? (str lhs "=" rhs))))))
 
-(deftest recognize-c-jump-instructions
+(deftest recognize-jump-instructions
   "Checks whether all possible c-jump-instructions are recognized."
   (doseq [lhs ["null" "M" "D" "MD" "A" "AM" "AD" "AMD"]]
     (doseq [rhs  ["null" "JGT" "JEQ" "JGE" "JLT" "JNE" "JLE" "JMP"]]
-      (is (is-c-jump-instruction? (str lhs ";" rhs))))))
+      (is (jump-instruction? (str lhs ";" rhs))))))
 
-(deftest recognize-a-const-instructions
+(deftest recognize-const-instructions
   "Checks whether the classification of a-const instructions 
    into valid and invalid ones is correct"
-  (is (is-a-const-instruction? "@0"))
-  (is (is-a-const-instruction? "@1"))
-  (is (is-a-const-instruction? "@42"))
-  (is (is-a-const-instruction? "@360"))
-  (is (is-a-const-instruction? "@1337"))
-  (is (not (is-a-const-instruction? "@-1")))
-  (is (not (is-a-const-instruction? "1")))
-  (is (not (is-a-const-instruction? "@LOOP")))
-  (is (not (is-a-const-instruction? "@"))))
+  (is (const-instruction? "@0"))
+  (is (const-instruction? "@1"))
+  (is (const-instruction? "@42"))
+  (is (const-instruction? "@360"))
+  (is (const-instruction? "@1337"))
+  (is (not (const-instruction? "@-1")))
+  (is (not (const-instruction? "1")))
+  (is (not (const-instruction? "@LOOP")))
+  (is (not (const-instruction? "@"))))
 
-(deftest recognize-a-symbol-instructions
+(deftest recognize-symbol-instructions
   "Checks whether the classification of a-symbol 
    instructions into valid and invalid ones is correct"
-  (is (is-a-symbol-instruction? "@LOOP"))
-  (is (is-a-symbol-instruction? "@foo"))
-  (is (is-a-symbol-instruction? "@R1"))
-  (is (is-a-symbol-instruction? "@ball.setdestination$if_true0"))
-  (is (is-a-symbol-instruction? "@END_EQ"))
-  (is (is-a-symbol-instruction? "@:_$Xy4"))
-  (is (is-a-symbol-instruction? "@_$Xy4:"))
-  (is (is-a-symbol-instruction? "@$Xy4:_"))
-  (is (is-a-symbol-instruction? "@Xy4:_$"))
-  (is (is-a-symbol-instruction? "@y4:_$X"))
-  (is (not (is-a-symbol-instruction? "@4:_$Xy")))
-  (is (not (is-a-symbol-instruction? "@2CHAINZ"))))
+  (is (symbol-instruction? "@LOOP"))
+  (is (symbol-instruction? "@foo"))
+  (is (symbol-instruction? "@R1"))
+  (is (symbol-instruction? "@ball.setdestination$if_true0"))
+  (is (symbol-instruction? "@END_EQ"))
+  (is (symbol-instruction? "@:_$Xy4"))
+  (is (symbol-instruction? "@_$Xy4:"))
+  (is (symbol-instruction? "@$Xy4:_"))
+  (is (symbol-instruction? "@Xy4:_$"))
+  (is (symbol-instruction? "@y4:_$X"))
+  (is (not (symbol-instruction? "@4:_$Xy")))
+  (is (not (symbol-instruction? "@2CHAINZ"))))
 
 (run-tests)

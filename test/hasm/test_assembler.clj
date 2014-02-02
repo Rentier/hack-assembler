a43a3a0d56f30a7bdd73cbde6ecfb5ddb3e2ce75
Jan-Christoph Klie <rentier42@googlemail.com>
2014-02-02 20:28:01 +0100

    Refactor tests

--- test/hasm/test_assembler.clj
+++ test/hasm/test_assembler.clj
@@ -11,25 +11,25 @@
   (with-open [rdr (-> f io/resource io/file io/reader)]
     (doall (line-seq rdr))))
 
-(deftest generate-add
-  ""
-  (is (= (-> "add/Add.asm" get-tokens-from-resource generate-bytecode )
-         (get-binary-from-resource "add/Add.hack"))))
+(defn code-identical? [asm hack]
+  (= (-> asm get-tokens-from-resource generate-bytecode )
+     (get-binary-from-resource hack)))
 
-(deftest generate-max-nosym
-  ""
-  (is (= (-> "max/MaxL.asm" get-tokens-from-resource generate-bytecode )
-         (get-binary-from-resource "max/Max.hack"))))
 
-(deftest generate-rect-nosym
-  ""
-  (is (= (-> "rect/RectL.asm" get-tokens-from-resource generate-bytecode )
-         (get-binary-from-resource "rect/Rect.hack"))))
+(deftest generate-binary-from-source-nosym
+  "Given an *.asm file with symbols already replaced, check
+   whether the generated binary code is identical to a known
+   to be correct version"
+  (is (code-identical? "add/Add.asm" "add/Add.hack"))
+  (is (code-identical? "max/MaxL.asm" "max/Max.hack"))
+  (is (code-identical? "rect/RectL.asm" "rect/Rect.hack"))
+  (is (code-identical? "pong/PongL.asm" "pong/Pong.hack")))
 
-(deftest generate-pong-nosym
-  ""
-  (is (= (-> "pong/PongL.asm" get-tokens-from-resource generate-bytecode )
-         (get-binary-from-resource "pong/Pong.hack"))))
+(deftest generate-binary-from-source
+  "Given an *.asm file, check whether the generated binary 
+   code is identical to a known to be correct version"
+  (is (code-identical? "max/Max.asm" "max/Max.hack")))
+  
 
 
 (run-tests)
